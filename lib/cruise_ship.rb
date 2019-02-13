ee74d9ba41c198aa0034b08494c46028b3a78d11
roy renno <royrenno@yahoo.com>
2019-02-13 02:31:36 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,9 +8,9 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |key, value|
-    if key == :suite_a && value.start_with?("A")
-    winner = value
+  passengers.each do |key, name|
+    if key == :suite_a && name.start_with?("A")
+    winner = name
   end
 end
 winner
